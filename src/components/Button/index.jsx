920a4dbadb3d082cae5ef5b1aacffd798e6b8c0c
Konrad <jupterkdh@gmail.com>
2023-05-31 16:37:01 -0300

    added Button component

---
+++ src/components/Button/index.jsx
@@ -0,0 +1,20 @@
+import { TbReceipt } from "react-icons/tb";
+import { useMediaQuery } from 'react-responsive';
+
+import { Container } from "./styles";
+
+export function Button({title, loading = false, isClient, ...rest}) {
+  const isDesktop = useMediaQuery({minWidth: 1024});
+
+  return (
+    <Container
+      type="button"
+      disabled={loading}
+      {...rest}
+    >
+     {isClient && <TbReceipt size={"3.2rem"}/>}
+     {loading ? "Carregando..." : title}
+     {isClient && <span>{isDesktop ? `(&{rest.orderCount})` : rest.orderCount}</span>} 
+    </Container>
+  );
+}
