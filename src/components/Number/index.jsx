cf6dfeac1f77d925fd3b704916361ee60d2c080f
Konrad <jupterkdh@gmail.com>
2023-06-01 17:41:25 -0300

    Number, Search, Section, Tag, and TextArea components completed

---
+++ src/components/Number/index.jsx
@@ -0,0 +1,26 @@
+import { FiMinus, FiPlus } from "react-icons/fi";
+import { useState } from "react";
+
+import { Container } from "./styles";
+
+export function Number() {
+  const [number, setNumber] = useState(1);
+
+  const incrementNumber = () => {
+    setNumber(number + 1);
+  };
+
+  const decrementNumber = () => {
+    if (number > 1) {
+      setNumber(number - 1);
+    }
+  };
+
+  return (
+    <Container>
+      <button onClick={decrementNumber}><FiMinus /></button>
+      <span>{number < 10 ? `0${number}` : number}</span>
+      <button onClick={incrementNumber}><FiPlus /></button>
+    </Container>
+  );
+}
