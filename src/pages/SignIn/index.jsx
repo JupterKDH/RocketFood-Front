fc14ade432cbcbbd575903e32a82c7cc542e61f8
Konrad <jupterkdh@gmail.com>
2023-06-05 14:11:17 -0300

    create files for pages, SignIn completed

---
+++ src/pages/SignIn/index.jsx
@@ -0,0 +1,56 @@
+import { useState } from 'react';
+import { Link } from "react-router-dom";
+
+import { useAuth } from '../../hooks/auth';
+
+import { Container, Form, Brand } from "./styles";
+import brand from "../../assets/brand.svg";
+
+import { Section } from '../../components/Section';
+import { Button } from "../../components/Button";
+import { Input } from '../../components/Input';
+
+export function SignIn() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const {signIn} = useAuth();
+
+  function handleSignIn(){
+    signIn({email, password});
+  }
+
+  return (
+    <Container>
+      <Brand>
+        <img src={brand} alt="Logo" />
+      </Brand>
+
+      <Form>
+        <h2>Faça seu login</h2>
+
+        <Section title="Email">
+          <Input 
+            placeholder="Exemplo: exemplo@exemplo.com.br" 
+            type="text"
+            onChange={e => setEmail(e.target.value)}
+          />
+        </Section>
+
+        <Section title="Senha">
+          <Input 
+            placeholder="No mínimo 6 caracteres" 
+            type="password"
+            onChange={e => setPassword(e.target.value)}
+          />
+        </Section>
+
+        <Button title="Entrar" onClick={handleSignIn} />
+
+        <Link to="/register">
+          Criar uma conta
+        </Link>
+      </Form>
+    </Container>
+  );
+}
